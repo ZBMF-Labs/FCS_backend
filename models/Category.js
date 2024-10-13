16b4632deb7544c932d1a4c4b698e95b74861436
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-10-13 15:45:14 -0300

    feat: add Category model with associations
    - Defined the Category model with fields: id, userId, name, and description.
    - Added foreign key reference from userId to USERS table.
    - Set up unique constraint on the name field and enforced NOT NULL for required fields.
    - Configured cascade update and delete behaviors for the userId foreign key.

---
+++ models/Category.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize'
+
+export default (sequelize) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'USERS',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    },
+  )
+
+  return Category
+}
