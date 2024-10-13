338c89b8330a81bb77707d3021f5b91d2cb0c1c9
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-10-13 15:52:21 -0300

    feat: add Account model with associations
    - Defined the Account model with fields: id, userId, name, balance, and description.
    - Added foreign key constraint on userId referencing the USERS table.
    - Set unique constraint on the name field and enforced NOT NULL for required fields.
    - Configured default balance to 0.00 and set up cascade behaviors for userId foreign key.

---
+++ models/Account.js
@@ -0,0 +1,43 @@
+import { DataTypes } from 'sequelize'
+
+export default (sequelize) => {
+  const Account = sequelize.define(
+    'Account',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'USERS',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      balance: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    },
+  )
+
+  return Account
+}
