789749fb9b3d6405c8272bee27ca6fbc5c1988bf
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-10-13 15:54:22 -0300

    feat: add migration for Accounts table
    - Created migration to define the Accounts table with columns: id, userId, name, balance, and description.
    - Added foreign key constraint on userId referencing the USERS table.
    - Set unique constraint on the name column and enforced NOT NULL for required fields.
    - Configured default balance to 0.00 and set up cascade behaviors for userId foreign key.

---
+++ migrations/20241013185322-create-accounts.cjs
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ACCOUNTS', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'USERS',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      balance: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      description: {
+        type: Sequelize.STRING(255),
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('ACCOUNTS')
+  },
+}
