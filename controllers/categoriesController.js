afb97b0b1fc153a346d2e4a0e8ce83ae52dbc76d
richardbmezzomo <richardmezzomo1@gmail.com>
2025-02-16 22:29:56 -0300

    feat: add UPDATE category

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -48,3 +48,28 @@ export const getCategories = async (req, res) => {
     return res.status(500).json({ messagem: 'Erro ao buscar categoria' })
   }
 }
+
+export const updateCategories = async (req, res) => {
+  try {
+    const { name } = req.body
+    const userId = req.user.userId
+    const { id } = req.params
+
+    const category = await Category.findOne({ where: { id, userId } })
+
+    console.log('Resultado da busca:', category)
+
+    if (!category) {
+      return res.status(404).json({ message: 'Categoria não encontrada.' })
+    }
+
+    category.name = name
+    await category.save()
+
+    return res
+      .status(200)
+      .json({ message: 'Categoria atualizada com sucesso!' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao atualizar categoria!' })
+  }
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 import {
   createCategory,
   getCategories,
+  updateCategories,
 } from '../controllers/categoriesController.js'
 import { authenticateUser } from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 router.post('/', authenticateUser, createCategory)
 router.get('/', authenticateUser, getCategories)
+router.put('/:id', authenticateUser, updateCategories)
 
 export default router
